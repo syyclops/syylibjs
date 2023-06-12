5e0213f20da14dcd210b5130c7bd3f462473000d
varunbsyyclops <varun.bardwaj@syyclops.com>
2023-06-12 16:11:36 +0530

    Add: Toggle

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import classNames from "classnames";
+import { ToggleProps } from "../types/toggle";
+import { variants } from "../config";
+import { sizes } from "../config/toggle";
+
+const Toggle = ({
+  variant = "primary",
+  size = "sm",
+  onAction,
+}: ToggleProps) => {
+  const [slide, setSlide] = useState(false);
+
+  return (
+    <div
+      className={classNames(
+        "relative",
+        "rounded-full",
+        "cursor-pointer",
+        "flex items-center",
+        variants[variant],
+        sizes[size].toggle
+      )}
+      onClick={() => {
+        setSlide(!slide);
+        onAction(!slide);
+      }}
+    >
+      <div
+        className={classNames(
+          "absolute",
+          "bg-white",
+          "rounded-full",
+          "m-[0.1rem]",
+          "transition",
+          "duration-150",
+          !slide ? "translate-x-[1%]" : "translate-x-[90%]",
+          sizes[size].switch
+        )}
+      />
+    </div>
+  );
+};
+
+export default Toggle;
