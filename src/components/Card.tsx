fc632744d2d140f4da30100f681d2461c636fb65
varunbsyyclops <varun.bardwaj@syyclops.com>
2023-06-28 08:20:45 +0530

    feat(new-feature): add card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+import classnames from "classnames";
+import { CardProps } from "../types/card";
+
+const Card = ({
+  children,
+  width = "w-full",
+  height = "h-auto",
+  cx = "",
+}: CardProps) => {
+  return (
+    <div
+      className={twMerge(
+        classnames(
+          "bg-dark-neutral-100",
+          "text-light-neutral-300",
+          "border border-mid-neutral-300",
+          "rounded-xl",
+          width,
+          height
+        ),
+        cx
+      )}
+      style={{
+        border: "1px solid",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
